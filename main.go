357f18cb8164497af3abae9af412fa77516bdad0
Bryan Jarvis <bjarvis@laughingj.com>
2018-06-13 20:16:54 -0500

    chapter 2.3a

--- main.go
+++ main.go
@@ -8,6 +8,16 @@ import (
 // Define a Home function which writes a plain-text "Hello from Snippetbox"
 // message as the HTTP response body.
 func Home(w http.ResponseWriter, r *http.Request) {
+	// Use r.URL.Path to check whether the request URL path exactly matches "/".
+	// If it doesn't, use the w.WriteHeader() method to send a 404 status code and
+	// the w.Write() method to write a "Not Found" response body. Importantly, we
+	// then return from the function so that the subsequent code is not executed.
+	if r.URL.Path != "/" {
+		w.WriteHeader(404)
+		w.Write([]byte("Not Found"))
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
