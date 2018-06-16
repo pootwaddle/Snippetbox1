49c0a106ad4a803929f3222fe5b5f19eb923616c
Bryan Jarvis <bjarvis@laughingj.com>
2018-06-15 21:04:23 -0500

    Chapter 2.5 Routing Requests

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -12,3 +12,13 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("Hello from Snippetbox"))
 }
+
+// Add a placeholder ShowSnippet handler function.
+func ShowSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// Add a placeholder NewSnippet handler function.
+func NewSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display the new snippet form..."))
+}
