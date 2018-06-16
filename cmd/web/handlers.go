56360fd21bffe7e20a8c7968696f1e761c1a5adb
Bryan Jarvis <bjarvis@laughingj.com>
2018-06-15 21:21:01 -0500

    Chapter 2.6 URL Query Strings

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +17,19 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 // Add a placeholder ShowSnippet handler function.
 func ShowSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function. If it couldn't
+	// be converted to an integer, or the value is less than 1, we return a 404
+	// Not Found response.
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Use the fmt.Fprintf() function to interpolate the id value with our response
+	// and write it to the http.ResponseWriter.
+	fmt.Fprintf(w, "Display a specific snippet (ID %d)...", id)
 }
 
 // Add a placeholder NewSnippet handler function.
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,8 +8,8 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Home)
-  mux.HandleFunc("/snippet", ShowSnippet)
-  mux.HandleFunc("/snippet/new", NewSnippet)
+	mux.HandleFunc("/snippet", ShowSnippet)
+	mux.HandleFunc("/snippet/new", NewSnippet)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
