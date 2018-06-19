154734f081649a23d1f7f0469cf1ed885c3067f6
Bryan Jarvis <bjarvis@laughingj.com>
2018-06-19 18:10:00 -0500

    Chapter 3.1 Command line flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Define command-line flags for the network address and location of the static
+	// files directory.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	staticDir := flag.String("static-dir", "./ui/static", "Path to static assets")
+
+	// Importantly, we use the flag.Parse() function to parse the command-line flags.
+	// This reads in the command-line flag values and assigns them to the addr and
+	// staticDir variables. You need to parse the flags *before* you use the addr
+	// or staticDir variables, otherwise they will always contain the default value.
+	// If any errors are encountered during parsing the application will be
+	// terminated.
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Home)
 	mux.HandleFunc("/snippet", ShowSnippet)
 	mux.HandleFunc("/snippet/new", NewSnippet)
 
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// As before, the path given to the http.Dir function is relative to our project
-	// repository root.
-	fileServer := http.FileServer(http.Dir("./ui/static"))
+	// The value returned from the flag.String() function is a pointer to the flag
+	// value, not the value itself. So we need to dereference the pointer (i.e.
+	// prefix it with the * symbol) before we use it as the path for our static file
+	// server.
+	fileServer := http.FileServer(http.Dir(*staticDir))
 
 	// Use the mux.Handle() function to register the file server as the
 	// handler for all URL paths that start with "/static/". For matching
@@ -22,7 +37,10 @@ func main() {
 	// server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	// Again, we dereference the addr variable and use it as the network address
+	// to listen on. Notice that we also use the log.Printf() function to interpolate
+	// the correct address in the log message.
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
