375977d94fe7ebfdd9f17266502ee26d91697b31
Bryan Jarvis <bjarvis@laughingj.com>
2018-06-30 16:43:28 -0500

    Chapter 4.3 Designing A Database Module

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,23 +24,6 @@ func main() {
 		StaticDir: *staticDir,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.Home)
-	mux.HandleFunc("/snippet", app.ShowSnippet)
-	mux.HandleFunc("/snippet/new", app.NewSnippet)
-
-	// The value returned from the flag.String() function is a pointer to the flag
-	// value, not the value itself. So we need to dereference the pointer (i.e.
-	// prefix it with the * symbol) before we use it as the path for our static file
-	// server.
-	fileServer := http.FileServer(http.Dir(*staticDir))
-
-	// Use the mux.Handle() function to register the file server as the
-	// handler for all URL paths that start with "/static/". For matching
-	// paths, we strip the "/static" prefix before the request reaches the file
-	// server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	// Again, we dereference the addr variable and use it as the network address
 	// to listen on. Notice that we also use the log.Printf() function to interpolate
 	// the correct address in the log message.
